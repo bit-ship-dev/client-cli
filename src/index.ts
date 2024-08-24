bfd8009f3d48cdfc9c4f43ce18e8f4961a97a772
matusporuben <matus.poruben@gmail.com>
2024-08-24 11:48:34 +0200

    init services on start of client clie

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,24 @@
 import { defineCommand, runMain } from "citty";
-
+import {setupConsola} from "./services/consola";
+import {setupConfig} from "./services/config";
+import {setupStorage} from "./services/storage";
 
 
 const main = defineCommand({
   meta: {
-    name: "bship-cli",
+    name: "bit-ship",
     version: "1.0.0",
-    description: "Bitship cli",
+    description: "Bit-Ship CLI https://bit-ship.dev/",
   },
   subCommands: {
     analyse: () => import("./modules/analyse/analyse").then((r) => r.default),
     run:() => import("./modules/run/run").then((r) => r.default),
+    settings:() => import("./modules/settings/settings").then((r) => r.default),
   }
 });
 
+
+setupStorage();
+setupConfig('./');
+setupConsola();
 runMain(main);
